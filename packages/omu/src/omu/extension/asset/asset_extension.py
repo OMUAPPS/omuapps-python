ca476a93851f5a05442f6946a9a1ab9f22fe3ef5
am230 <111672334+am230@users.noreply.github.com>
2024-02-13 20:29:48 +0900

    enhance(omu): AssetExtensionをバイトデータに対応させた

--- packages/omu/src/omu/extension/asset/asset_extension.py
+++ packages/omu/src/omu/extension/asset/asset_extension.py
@@ -1,23 +1,46 @@
-import base64
 from typing import Dict, List
 
 from omu.client.client import Client
 from omu.connection.connection import ConnectionListener
-from omu.extension.endpoint import JsonEndpointType
+from omu.extension.endpoint import SerializeEndpointType
 from omu.extension.extension import Extension, define_extension_type
+from omu.helper import ByteReader, ByteWriter
+from omu.interface import Serializable
+from omu.interface.serializable import Serializer
 
 AssetExtensionType = define_extension_type(
     "asset",
     lambda client: AssetExtension(client),
     lambda: [],
 )
+type Files = Dict[str, bytes]
 
 
-type b64str = str
+class FileSerializer(Serializable[Files, bytes]):
+    def serialize(self, data: Files) -> bytes:
+        writer = ByteWriter()
+        writer.write_int(len(data))
+        for key, value in data.items():
+            writer.write_string(key)
+            writer.write_byte_array(value)
+        return writer.finish()
 
-AssetUploadEndpoint = JsonEndpointType[Dict[str, b64str], List[str]].of_extension(
+    def deserialize(self, data: bytes) -> Files:
+        reader = ByteReader(data)
+        length = reader.read_int()
+        files = {}
+        for _ in range(length):
+            key = reader.read_string()
+            value = reader.read_byte_array()
+            files[key] = value
+        return files
+
+
+AssetUploadEndpoint = SerializeEndpointType[Files, List[str]].of_extension(
     AssetExtensionType,
     "upload",
+    request_serializer=FileSerializer(),
+    response_serializer=Serializer.json(),
 )
 
 
@@ -26,10 +49,5 @@ class AssetExtension(Extension, ConnectionListener):
         self.client = client
         client.connection.add_listener(self)
 
-    async def upload(self, assets: Dict[str, bytes]) -> List[str]:
-        return await self.client.endpoints.call(
-            AssetUploadEndpoint, {k: self._encode(v) for k, v in assets.items()}
-        )
-
-    def _encode(self, data: bytes) -> b64str:
-        return base64.b64encode(data).decode("utf-8")
+    async def upload(self, assets: Files) -> List[str]:
+        return await self.client.endpoints.call(AssetUploadEndpoint, assets)
--- packages/server/src/omuserver/extension/asset/asset_extension.py
+++ packages/server/src/omuserver/extension/asset/asset_extension.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
-import base64
-from typing import TYPE_CHECKING, Dict, List
+from typing import TYPE_CHECKING, List
 
-from omu.extension.asset.asset_extension import AssetUploadEndpoint
+from omu.extension.asset.asset_extension import AssetUploadEndpoint, Files
 
 from omuserver.extension import Extension
 from omuserver.helper import safe_path_join
@@ -16,17 +15,16 @@ if TYPE_CHECKING:
 class AssetExtension(Extension):
     def __init__(self, server: Server) -> None:
         self._server = server
-        server.endpoints.bind_endpoint(AssetUploadEndpoint, self._on_upload)
+        server.endpoints.bind_endpoint(AssetUploadEndpoint, self.handle_upload)
 
-    async def _on_upload(self, session: Session, files: Dict[str, str]) -> List[str]:
-        for key, data in files.items():
-            path = safe_path_join(self._server.directories.assets, key)
-            path.parent.mkdir(parents=True, exist_ok=True)
-            path.write_bytes(self._decode(data))
-        return list(files.keys())
-
-    def _decode(self, data: str) -> bytes:
-        return base64.b64decode(data.encode("utf-8"))
+    async def handle_upload(self, session: Session, files: Files) -> List[str]:
+        uploaded_files = []
+        for file_name, file_data in files.items():
+            file_path = safe_path_join(self._server.directories.assets, file_name)
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+            file_path.write_bytes(file_data)
+            uploaded_files.append(file_name)
+        return uploaded_files
 
     @classmethod
     def create(cls, server: Server) -> AssetExtension:
