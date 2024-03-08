e717b7414ab897f5bb7f45162e256e9f06847c6e
am230 <111672334+am230@users.noreply.github.com>
2024-03-09 00:02:21 +0900

    feat(server): pluginの依存関係をインストール出来る機能

--- packages/server/src/omuserver/extension/plugin/plugin_extension.py
+++ packages/server/src/omuserver/extension/plugin/plugin_extension.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 import asyncio
 import json
 
+from importlib.util import find_spec
 from pathlib import Path
+import re
+import subprocess
+import sys
 import threading
 from typing import (
     TYPE_CHECKING,
     Dict,
-    List,
     NotRequired,
     Protocol,
     TypeGuard,
@@ -28,7 +31,7 @@ class Plugin(Protocol):
 
 
 class PluginMetadata(TypedDict):
-    dependencies: List[str]
+    dependencies: Dict[str, str]
     module: str
     isolated: NotRequired[bool]
 
@@ -36,7 +39,7 @@ class PluginMetadata(TypedDict):
 class PluginExtension(Extension, ServerListener):
     def __init__(self, server: Server) -> None:
         self._server = server
-        self.plugins: Dict[str, Plugin] = {}
+        self.plugins: Dict[str, PluginMetadata] = {}
         server.add_listener(self)
 
     @classmethod
@@ -53,7 +56,50 @@ class PluginExtension(Extension, ServerListener):
             if plugin.name.startswith("_"):
                 continue
             logger.info(f"Loading plugin: {plugin.name}")
-            await self._load_plugin(plugin)
+            metadata = self._load_plugin(plugin)
+            self.plugins[metadata["module"]] = metadata
+        await self.install_dependencies()
+        await self.run_plugins()
+
+    def _load_plugin(self, path: Path) -> PluginMetadata:
+        metadata = PluginMetadata(**json.loads(path.read_text()))
+        invalid_dependencies = []
+        for dependency in metadata.get("dependencies", []):
+            if not re.match(r"^[a-zA-Z0-9_-]+$", dependency):
+                invalid_dependencies.append(dependency)
+        if invalid_dependencies:
+            raise ValueError(f"Invalid dependencies in {path}: {invalid_dependencies}")
+        if not re.match(r"^[a-zA-Z0-9_-]+$", metadata["module"]):
+            raise ValueError(f"Invalid module in {path}: {metadata['module']}")
+        return metadata
+
+    async def install_dependencies(self) -> None:
+        # https://stackoverflow.com/a/44210735
+        dependencies: dict[str, str] = {}
+        for metadata in self.plugins.values():
+            dependencies.update(metadata["dependencies"])
+        to_install: Dict[str, str] = {}
+        for dependency, version in dependencies.items():
+            spec = find_spec(dependency)
+            if spec is None:
+                to_install[dependency] = version
+        if len(to_install) == 0:
+            return
+        logger.info(f"Installing dependencies {to_install}")
+        subprocess.run(
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                *to_install,
+            ],
+            check=True,
+        )
+
+    async def run_plugins(self) -> None:
+        for metadata in self.plugins.values():
+            await self.run_plugin(metadata)
 
     def validate_plugin(self, plugin: Plugin) -> TypeGuard[Plugin]:
         main = getattr(plugin, "main", None)
@@ -63,8 +109,7 @@ class PluginExtension(Extension, ServerListener):
             raise ValueError(f"Plugin {plugin} does not have a main coroutine")
         return True
 
-    async def _load_plugin(self, path: Path) -> None:
-        metadata = PluginMetadata(**json.loads((path).read_text()))
+    async def run_plugin(self, metadata: PluginMetadata):
         plugin = __import__(metadata["module"])
         if not self.validate_plugin(plugin):
             return
@@ -72,7 +117,9 @@ class PluginExtension(Extension, ServerListener):
             loop = asyncio.new_event_loop()
             loop.create_task(plugin.main())
             thread = threading.Thread(
-                target=loop.run_forever, daemon=True, name=f"Plugin {path.name}"
+                target=loop.run_forever,
+                daemon=True,
+                name=f"Plugin {metadata['module']}",
             )
             thread.start()
         else:
