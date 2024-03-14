6140a304ec2ae2ddde9970319a355962b588f6a8
am230 <111672334+am230@users.noreply.github.com>
2024-03-15 05:25:40 +0900

    fix: 古いlistenerの書き方の残骸を削除

--- packages/omu/src/omu/network/__init__.py
+++ packages/omu/src/omu/network/__init__.py
@@ -1,12 +1,11 @@
 from .address import Address
-from .network import Network, NetworkListeners, NetworkStatus
+from .network import Network, NetworkStatus
 from .packet import Packet, PacketType
 
 __all__ = [
     "Address",
     "Network",
     "NetworkStatus",
-    "NetworkListeners",
     "Network",
     "Packet",
     "PacketType",
