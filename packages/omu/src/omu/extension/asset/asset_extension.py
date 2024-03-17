50623ebc8eaf280b253184c56f0439f945043968
am230 <111672334+am230@users.noreply.github.com>
2024-03-17 14:26:57 +0900

    feat: asset-extensionのuploadを更新しdownloadを追加

--- packages/omu/src/omu/extension/asset/asset_extension.py
+++ packages/omu/src/omu/extension/asset/asset_extension.py
@@ -3,6 +3,8 @@ from typing import Dict, List
 from omu.client import Client
 from omu.extension import Extension, ExtensionType
 from omu.extension.endpoint import EndpointType
+from omu.helper import instance
+from omu.identifier import Identifier
 from omu.network.bytebuffer import ByteReader, ByteWriter
 from omu.serializer import Serializable, Serializer
 
@@ -11,34 +13,44 @@ AssetExtensionType = ExtensionType(
     lambda client: AssetExtension(client),
     lambda: [],
 )
-type Files = Dict[str, bytes]
+type Files = Dict[Identifier, bytes]
 
 
-class FilesSerializer(Serializable[Files, bytes]):
+@instance
+class FILES_SERIALIZER(Serializable[Files, bytes]):
     def serialize(self, data: Files) -> bytes:
         writer = ByteWriter()
         writer.write_int(len(data))
-        for key, value in data.items():
-            writer.write_string(key)
+        for identifier, value in data.items():
+            writer.write_string(identifier.key())
             writer.write_byte_array(value)
         return writer.finish()
 
     def deserialize(self, data: bytes) -> Files:
         with ByteReader(data) as reader:
-            length = reader.read_int()
+            count = reader.read_int()
             files: Files = {}
-            for _ in range(length):
-                key = reader.read_string()
+            for _ in range(count):
+                identifier = Identifier.from_key(reader.read_string())
                 value = reader.read_byte_array()
-                files[key] = value
+                files[identifier] = value
         return files
 
 
-AssetUploadEndpoint = EndpointType[Files, List[str]].create_serialized(
+IDENTIFIERS_SERIALIZER = Serializer(Identifier.key, Identifier.from_key).array().json()
+
+
+AssetUploadEndpoint = EndpointType[Files, List[Identifier]].create_serialized(
     AssetExtensionType,
     "upload",
-    request_serializer=FilesSerializer(),
-    response_serializer=Serializer.json(),
+    request_serializer=FILES_SERIALIZER,
+    response_serializer=IDENTIFIERS_SERIALIZER,
+)
+AssetDownloadEndpoint = EndpointType[List[Identifier], Files].create_serialized(
+    AssetExtensionType,
+    "download",
+    request_serializer=IDENTIFIERS_SERIALIZER,
+    response_serializer=FILES_SERIALIZER,
 )
 
 
@@ -46,5 +58,8 @@ class AssetExtension(Extension):
     def __init__(self, client: Client) -> None:
         self.client = client
 
-    async def upload(self, assets: Files) -> List[str]:
+    async def upload(self, assets: Files) -> List[Identifier]:
         return await self.client.endpoints.call(AssetUploadEndpoint, assets)
+
+    async def download(self, identifiers: List[Identifier]) -> Files:
+        return await self.client.endpoints.call(AssetDownloadEndpoint, identifiers)
--- packages/server/src/omuserver/extension/asset/asset_extension.py
+++ packages/server/src/omuserver/extension/asset/asset_extension.py
@@ -2,7 +2,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, List
 
-from omu.extension.asset.asset_extension import AssetUploadEndpoint, Files
+from omu.extension.asset.asset_extension import (
+    AssetDownloadEndpoint,
+    AssetUploadEndpoint,
+    Files,
+)
+from omu.identifier import Identifier
 
 from omuserver.helper import safe_path_join
 
@@ -15,12 +20,25 @@ class AssetExtension:
     def __init__(self, server: Server) -> None:
         self._server = server
         server.endpoints.bind_endpoint(AssetUploadEndpoint, self.handle_upload)
+        server.endpoints.bind_endpoint(AssetDownloadEndpoint, self.handle_download)
 
-    async def handle_upload(self, session: Session, files: Files) -> List[str]:
+    async def handle_upload(self, session: Session, files: Files) -> List[Identifier]:
         uploaded_files = []
-        for file_name, file_data in files.items():
-            file_path = safe_path_join(self._server.directories.assets, file_name)
+        for file_identifier, file_data in files.items():
+            path = file_identifier.to_path()
+            file_path = safe_path_join(self._server.directories.assets, path)
             file_path.parent.mkdir(parents=True, exist_ok=True)
             file_path.write_bytes(file_data)
-            uploaded_files.append(file_name)
+            uploaded_files.append(file_identifier)
         return uploaded_files
+
+    async def handle_download(
+        self, session: Session, identifiers: List[Identifier]
+    ) -> Files:
+        files = {}
+        for identifier in identifiers:
+            path = identifier.to_path()
+            file_path = safe_path_join(self._server.directories.assets, path)
+            if file_path.exists():
+                files[identifier] = file_path.read_bytes()
+        return files
