d76392a7be82ce40a4b4165df461b0a7f0c4c913
am230 <111672334+am230@users.noreply.github.com>
2024-03-19 18:47:10 +0900

    refactor(chat): モデルのjsonの型情報をexportしないように変更

--- packages/chat/src/omuchat/model/__init__.py
+++ packages/chat/src/omuchat/model/__init__.py
@@ -1,33 +1,25 @@
 from . import content
-from .author import Author, AuthorJson
-from .channel import Channel, ChannelJson
-from .gift import Gift, GiftJson
-from .message import Message, MessageJson
-from .paid import Paid, PaidJson
-from .provider import Provider, ProviderJson
-from .role import MODERATOR, OWNER, VERIFIED, Role, RoleJson
-from .room import Room, RoomJson, RoomMetadata
+from .author import Author
+from .channel import Channel
+from .gift import Gift
+from .message import Message
+from .paid import Paid
+from .provider import Provider
+from .role import MODERATOR, OWNER, VERIFIED, Role
+from .room import Room, RoomMetadata
 
 __all__ = [
     "Author",
-    "AuthorJson",
     "Channel",
-    "ChannelJson",
     "Gift",
-    "GiftJson",
     "Message",
-    "MessageJson",
     "Paid",
-    "PaidJson",
     "Provider",
-    "ProviderJson",
     "Role",
-    "RoleJson",
     "MODERATOR",
     "OWNER",
     "VERIFIED",
     "Room",
-    "RoomJson",
     "RoomMetadata",
     "content",
 ]
