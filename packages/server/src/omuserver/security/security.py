abff6b73487a5514fb41336d4a152a4b851ab9d7
am230 <111672334+am230@users.noreply.github.com>
2024-03-30 20:54:34 +0900

    enhance(server): トークンの保存db構造を変更

--- packages/server/src/omuserver/security/security.py
+++ packages/server/src/omuserver/security/security.py
@@ -1,5 +1,6 @@
 import abc
 import datetime
+import random
 import sqlite3
 
 from omu import App
@@ -17,17 +18,26 @@ class Security(abc.ABC):
     async def validate_app_token(self, app: App, token: Token) -> bool: ...
 
 
+class TokenGenerator:
+    def __init__(self):
+        self._chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+    def generate(self, length: int) -> str:
+        return "".join(random.choices(self._chars, k=length))
+
+
 class ServerAuthenticator(Security):
     def __init__(self, server: Server):
         self._server = server
+        self._token_generator = TokenGenerator()
         self._token_db = sqlite3.connect(
             server.directories.get("security") / "tokens.sqlite"
         )
         self._token_db.execute(
             """
             CREATE TABLE IF NOT EXISTS tokens (
+                token TEXT PRIMARY KEY,
                 identifier TEXT,
-                token TEXT,
                 created_at INTEGER,
                 last_used_at INTEGER
             )
@@ -35,17 +45,17 @@ class ServerAuthenticator(Security):
         )
 
     async def generate_app_token(self, app: App) -> Token:
-        token = app.identifier.key()
+        token = self._token_generator.generate(32)
         self._token_db.execute(
             """
-            INSERT INTO tokens (identifier, token, created_at, last_used_at)
+            INSERT INTO tokens (token, identifier, created_at, last_used_at)
             VALUES (?, ?, ?, ?)
             """,
             (
-                app.identifier.key(),
                 token,
-                datetime.datetime.now().timestamp(),
-                datetime.datetime.now().timestamp(),
+                app.identifier.key(),
+                datetime.datetime.now(),
+                datetime.datetime.now(),
             ),
         )
         self._token_db.commit()
@@ -54,12 +64,21 @@ class ServerAuthenticator(Security):
     async def validate_app_token(self, app: App, token: Token) -> bool:
         cursor = self._token_db.execute(
             """
-            SELECT created_at, last_used_at FROM tokens
-            WHERE identifier = ? AND token = ?
+            SELECT token
+            FROM tokens
+            WHERE token = ? AND identifier = ?
             """,
-            (app.identifier.key(), token),
+            (token, app.identifier.key()),
         )
         result = cursor.fetchone()
         if result is None:
             return False
+        self._token_db.execute(
+            """
+            UPDATE tokens
+            SET last_used_at = ?
+            WHERE token = ?
+            """,
+            (datetime.datetime.now(), token),
+        )
         return True
