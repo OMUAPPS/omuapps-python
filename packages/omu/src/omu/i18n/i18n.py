abb1193fb5a93b64f024fde2b1e70dae48a7043e
am230 <111672334+am230@users.noreply.github.com>
2024-04-06 23:57:56 +0900

    コメントを削除

--- packages/omu/src/omu/i18n/i18n.py
+++ packages/omu/src/omu/i18n/i18n.py
@@ -4,24 +4,3 @@ from .locale import Locale
 
 type LocalizedText = Mapping[Locale, str]
 type Translations = Mapping[str, Translations | LocalizedText]
-
-
-# foge: Translation = {
-#     "general": {
-#         "hello": {
-#             "en": "Hello",
-#             "fr": "Bonjour",
-#             "ja": "こんにちは",
-#         },
-#         "goodbye": {
-#             "en": "Goodbye",
-#             "fr": "Au revoir",
-#             "ja": "さようなら",
-#         },
-#         "aaaaa": {
-#             "bbb": {
-#                 "ja": "あああああ",
-#             },
-#         },
-#     },
-# }
