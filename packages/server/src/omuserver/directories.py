e6c7c1e2e867cc38e512c0d0be18e584446c1ed8
am230 <111672334+am230@users.noreply.github.com>
2024-04-11 14:42:03 +0900

    refactor(server): Directoriesから不要な関数を削除
    `.__repr__` <- dataclassなため不要
    `.to_json`, `.__post_init__`, `.__str__`

--- packages/server/src/omuserver/directories.py
+++ packages/server/src/omuserver/directories.py
@@ -23,25 +23,7 @@ class Directories:
         self.assets.mkdir(parents=True, exist_ok=True)
         self.plugins.mkdir(parents=True, exist_ok=True)
 
-    def to_json(self):
-        return {
-            "data": str(self.data),
-            "assets": str(self.assets),
-            "plugins": str(self.plugins),
-        }
-
     def get(self, name: LiteralString):
         path = self.data / name
         path.mkdir(parents=True, exist_ok=True)
         return path
-
-    def __post_init__(self):
-        self.data = pathlib.Path(self.data)
-        self.assets = pathlib.Path(self.assets)
-        self.plugins = pathlib.Path(self.plugins)
-
-    def __str__(self):
-        return f"Directories(data={self.data}, assets={self.assets}, plugins={self.plugins})"
-
-    def __repr__(self):
-        return str(self)
