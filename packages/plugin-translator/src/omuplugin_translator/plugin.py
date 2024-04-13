1a25dc1d9fe3b6a1a38431df672638cf88fe0a70
am230 <111672334+am230@users.noreply.github.com>
2024-04-14 01:07:01 +0900

    feat(plugin-translator): 翻訳を設定できるようにした

--- packages/plugin-translator/src/omuplugin_translator/plugin.py
+++ packages/plugin-translator/src/omuplugin_translator/plugin.py
@@ -1,16 +1,37 @@
 from __future__ import annotations
 
-from edgetrans import EdgeTranslator, Translator
+from typing import TypedDict
+
+from edgetrans import EdgeTranslator, Language, Translator
+from loguru import logger
 from omu.identifier import Identifier
 from omuchat import App, Client, content, model
 from omuchat.event.event_types import events
 
+IDENTIFIER = Identifier("cc.omuchat", "plugin-translator")
 APP = App(
-    Identifier("cc.omuchat", "plugin-translator"),
+    IDENTIFIER,
     version="0.1.0",
 )
 client = Client(APP)
+
 translator: Translator | None = None
+
+
+class TrasnlatorConfig(TypedDict):
+    active: bool
+    language: Language
+
+
+config = TrasnlatorConfig(active=False, language="ja")
+CONFIG_REGISTRY_TYPE = client.registry.create("config", config)
+
+
+@CONFIG_REGISTRY_TYPE.listen
+async def on_config_change(new_config: TrasnlatorConfig):
+    global config
+    config = new_config
+    logger.info(f"translator config updated: {config}")
 
 
 async def translate(component: content.Component) -> content.Component:
@@ -20,7 +41,7 @@ async def translate(component: content.Component) -> content.Component:
         sibling for sibling in component.iter() if isinstance(sibling, content.Text)
     ]
     translated = await translator.translate(
-        [text.text for text in texts if text.text], "ar"
+        [text.text for text in texts if text.text], config["language"]
     )
     for text, (translation, _) in zip(texts, translated):
         text.text = translation
@@ -29,6 +50,8 @@ async def translate(component: content.Component) -> content.Component:
 
 @client.chat.messages.proxy
 async def on_message_add(message: model.Message) -> model.Message:
+    if not config["active"]:
+        return message
     if not message.content:
         return message
     message.content = await translate(message.content)
@@ -37,7 +60,8 @@ async def on_message_add(message: model.Message) -> model.Message:
 
 @client.on(events.ready)
 async def on_ready():
-    global translator
+    global translator, config
+    config = await CONFIG_REGISTRY_TYPE.get()
     translator = await EdgeTranslator.create()
 
 
