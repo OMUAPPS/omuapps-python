7465d104f6865f683c5069b88f30e85dc49f7969
am230 <111672334+am230@users.noreply.github.com>
2024-04-18 18:24:04 +0900

    perf(plugin-nyanya): 文字の置き換えにstr.translateを使うように

--- packages/plugin-nyanya/src/omuplugin_nyanya/plugin.py
+++ packages/plugin-nyanya/src/omuplugin_nyanya/plugin.py
@@ -19,7 +19,7 @@ async def translate(component: content.Component) -> content.Component:
     for child in component.iter():
         if not isinstance(child, content.Text):
             continue
-        child.text = "".join(replaces.get(char, char) for char in child.text)
+        child.text = child.text.translate(str.maketrans(replaces))
     return component
 
 
