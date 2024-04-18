564753493ee2ee5f1ba55ed0c40a5eede5316a3e
am230 <111672334+am230@users.noreply.github.com>
2024-04-18 23:20:10 +0900

    refactor(server): rename messages -> signals

--- packages/server/src/omuserver/extension/signal/signal_extension.py
+++ packages/server/src/omuserver/extension/signal/signal_extension.py
@@ -19,7 +19,7 @@ if TYPE_CHECKING:
 class SignalExtension:
     def __init__(self, server: Server):
         self._server = server
-        self.messages: DefaultDict[Identifier, List[Session]] = defaultdict(list)
+        self.signals: DefaultDict[Identifier, List[Session]] = defaultdict(list)
         server.packet_dispatcher.register(SIGNAL_LISTEN_PACKET, SIGNAL_BROADCAST_PACKET)
         server.packet_dispatcher.add_packet_handler(
             SIGNAL_LISTEN_PACKET, self.handle_listen
@@ -29,10 +29,10 @@ class SignalExtension:
         )
 
     def has(self, key):
-        return key in self.messages
+        return key in self.signals
 
     async def handle_listen(self, session: Session, identifier: Identifier) -> None:
-        listeners = self.messages[identifier]
+        listeners = self.signals[identifier]
         if session in listeners:
             return
 
@@ -40,6 +40,6 @@ class SignalExtension:
         session.listeners.disconnected += lambda session: listeners.remove(session)
 
     async def handle_broadcast(self, session: Session, data: SignalPacket) -> None:
-        listeners = self.messages[data.id]
+        listeners = self.signals[data.id]
         for listener in listeners:
             await listener.send(SIGNAL_BROADCAST_PACKET, data)
