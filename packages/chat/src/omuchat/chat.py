49949455c726e851db818e38de17a5fe7a89b433
am230 <111672334+am230@users.noreply.github.com>
2024-04-30 01:12:01 +0900

    Update chat.py

--- packages/chat/src/omuchat/chat.py
+++ packages/chat/src/omuchat/chat.py
@@ -2,74 +2,72 @@ from __future__ import annotations
 
 from typing import List
 
-from omu import Client, Identifier
+from omu import Client
 from omu.extension.endpoint import EndpointType
-from omu.extension.permission.permission import PermissionType
-from omu.extension.table import TableType
+from omu.extension.table import TablePermissions, TableType
 from omu.serializer import Serializer
 
+from omuchat.const import IDENTIFIER
 from omuchat.model.author import Author
 from omuchat.model.channel import Channel
 from omuchat.model.message import Message
 from omuchat.model.provider import Provider
 from omuchat.model.room import Room
-
-IDENTIFIER = Identifier.from_key("cc.omuchat:chat")
-CHAT_PERMISSION_TYPE = PermissionType(
-    IDENTIFIER / "chat",
-    metadata={
-        "level": "medium",
-        "name": {
-            "ja": "チャットのデータ",
-            "en": "Chat data",
-        },
-        "note": {
-            "ja": "チャットデータの読み書き",
-            "en": "Read and write chat data",
-        },
-    },
+from omuchat.permissions import (
+    CHAT_PERMISSION,
+    CHAT_READ_PERMISSION,
+    CHAT_WRITE_PERMISSION,
 )
-CHAT_PERMISSION = CHAT_PERMISSION_TYPE.id
-CHAT_READ_PERMISSION_TYPE = PermissionType(
-    IDENTIFIER / "chat" / "read",
-    metadata={
-        "level": "low",
-        "name": {
-            "ja": "チャットの読み取り",
-            "en": "Read chat",
-        },
-        "note": {
-            "ja": "チャットデータの読み取り",
-            "en": "Read chat data",
-        },
-    },
-)
-CHAT_READ_PERMISSION = CHAT_READ_PERMISSION_TYPE.id
 
 MESSAGE_TABLE = TableType.create_model(
     IDENTIFIER,
     "messages",
     Message,
+    permissions=TablePermissions(
+        all=CHAT_PERMISSION,
+        read=CHAT_READ_PERMISSION,
+        write=CHAT_WRITE_PERMISSION,
+    ),
 )
 AUTHOR_TABLE = TableType.create_model(
     IDENTIFIER,
     "authors",
     Author,
+    permissions=TablePermissions(
+        all=CHAT_PERMISSION,
+        read=CHAT_READ_PERMISSION,
+        write=CHAT_WRITE_PERMISSION,
+    ),
 )
 CHANNEL_TABLE = TableType.create_model(
     IDENTIFIER,
     "channels",
     Channel,
+    permissions=TablePermissions(
+        all=CHAT_PERMISSION,
+        read=CHAT_READ_PERMISSION,
+        write=CHAT_WRITE_PERMISSION,
+    ),
 )
 PROVIDER_TABLE = TableType.create_model(
     IDENTIFIER,
     "providers",
     Provider,
+    permissions=TablePermissions(
+        all=CHAT_PERMISSION,
+        read=CHAT_READ_PERMISSION,
+        write=CHAT_WRITE_PERMISSION,
+    ),
 )
 ROOM_TABLE = TableType.create_model(
     IDENTIFIER,
     "rooms",
     Room,
+    permissions=TablePermissions(
+        all=CHAT_PERMISSION,
+        read=CHAT_READ_PERMISSION,
+        write=CHAT_WRITE_PERMISSION,
+    ),
 )
 CREATE_CHANNEL_TREE_ENDPOINT = EndpointType[str, List[Channel]].create_json(
     IDENTIFIER,
